52d98b14fa5bb2da996f72affdf8ee93e7e1ad5e
fh <zhoushilei@jc-inter.com>
2017-07-30 19:10:52 +0800

    ddfdd

--- app/src/main/java/com/jc/zsl/text/MainActivity.java
+++ app/src/main/java/com/jc/zsl/text/MainActivity.java
@@ -2,7 +2,6 @@ package com.jc.zsl.text;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -13,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.all);
-        ImageView imageView = new ImageView(this);
         RelativeLayout relativeLayout;
         LinearLayout linearLayout = new LinearLayout(this);
     }
